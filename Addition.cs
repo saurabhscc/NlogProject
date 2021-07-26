a706845b4762119e6191433ddbba27cbf7f5502c
saurabhs <saurabhpshirsath@gmail.com>
2021-07-26 15:07:30 +0530

    [saurabh] Add; NLog Program Introduced

---
+++ Addition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NlogProject
+{
+    public class Addition
+    {
+        static NLog nlog = new NLog();
+        public static int add(int firstnumber,int secondnumber)
+        {
+            if (firstnumber == 0 || secondnumber == 0)
+            {
+                nlog.LogInfo("Add method");
+                nlog.LogDebug("Debug Successful");
+                nlog.LogError(" first number or second number is zero");
+                return 0;
+            }
+               
+            int result = firstnumber + secondnumber;
+            nlog.LogInfo("Add method");
+            nlog.LogDebug("Debug Successful");
+            nlog.LogWarn(" first number or second number is zero");
+            return result;
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NlogProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to NLog");
+            Addition obj = new Addition();
+            Console.WriteLine("Enter 2 numbers");
+            int firstnumber = Convert.ToInt32(Console.ReadLine());
+            int secondnumber = Convert.ToInt32(Console.ReadLine());
+            Addition.add(firstnumber,secondnumber);
+        }
+    }
+}
